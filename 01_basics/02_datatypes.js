c467cb54d0b6a301aa91cc729a8a2be1a3db9d64
Shivam Joshi <160034139+Shivamjoshi05@users.noreply.github.com>
2024-05-06 14:30:09 +0000

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -4,3 +4,22 @@
 
 console.log(3+3)
 
+let name = "shivam"
+let age = 18
+let isloggedon = true
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+//boolean => t or f
+//null => standlone value
+//undefined  => no value 
+//symbol => to find uniqueness
+
+
+// object 
+
+console.log(typeof null);
+
+
+
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,18 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+let isloggedin = ""
+let booleanislooged = Boolean(isloggedin)
+console.log(booleanislooged)
+
+// 1 => true ; 0 => false 
+// "" => false 
+// "shivam" => true
+
