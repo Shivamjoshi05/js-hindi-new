7db7a3ff72170b078249079b4df99728f7a50dfd
Shivam Joshi <160034139+Shivamjoshi05@users.noreply.github.com>
2024-05-07 16:40:25 +0530

    container config done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -8,6 +8,20 @@ let name = "shivam"
 let age = 18
 let isloggedon = true
 
+<<<<<<< HEAD
+// number => 2 to power 53
+//bigint 
+// string => ""
+//boolean => t or f
+//null => standlone value
+//undefined  => no value 
+//symbol => to find uniqueness
+
+
+// object 
+
+console.log(typeof"age");
+=======
 // number => 2 to power 53
 //bigint 
 // string => ""
@@ -23,3 +37,5 @@ console.log(typeof null);
 
 
 
+
+>>>>>>> fa6319273107e944e62e5089717cd122ae93c8ab
