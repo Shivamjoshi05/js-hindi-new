4cd23ea36401854577ecd282e49d22ca0cfb725c
Shivam Joshi <160034139+Shivamjoshi05@users.noreply.github.com>
2024-05-07 11:46:48 +0000

    change in location of file

---
+++ 01_basics/05_datatype-summery.js
@@ -0,0 +1,32 @@
+// primitive
+
+// 7 types : String , Number , Boolean , NUll , Undefined , Symbol , Bigint
+
+const score = 100
+const scorevalue = 100.3
+
+const isloggedin = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+// console.log(id === anotherid);
+
+// reference type (NON PRIMITIVE)
+
+// Array , Objects , Functions
+
+const hero = ["ironman","captian"]
+
+let myinfo = {
+    name:"shivam",
+    age:22,
+}
+
+const myfunction = function (){
+    console.log("hello world");
+}
+
+console.log(typeof myfunction);
